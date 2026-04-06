111279934dda77b718d5ed2ac35050ffe432cfbe
zero_Marty <osisovsky@gmail.com>
2026-04-06 19:14:07 +0200

    Initial commit

---
+++ Svetelnej_Radek/Svetelnej_Radek.ino
@@ -0,0 +1,19 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+//#define CLK
+//#define CS
+//#define SPI_DELAY 20
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+int main() {
+  // put your main code here, to run repeatedly:
+  while(1) {
+
+  }
+  return 32;
+}
