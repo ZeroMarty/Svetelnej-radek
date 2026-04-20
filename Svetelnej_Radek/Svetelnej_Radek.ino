aec4eeb251d2b27edae3a35419406c82f59364a5
zero_Marty <osisovsky@gmail.com>
2026-04-20 22:30:23 +0200

    dalsi sracky

--- Svetelnej_Radek/Svetelnej_Radek.ino
+++ Svetelnej_Radek/Svetelnej_Radek.ino
@@ -9,12 +9,15 @@
 #define SPI_DELAY 50
 #define pocet 12
 #define pocet_sloupcu (pocet * 8)
-int hodiny = 14;
-int minuty = 25;
-int sekundy = 30;
-int den = 21;
-int mesic = 4;
-int rok = 2026;
+uint8_t hodiny = 14;
+uint8_t minuty = 25;
+uint8_t sekundy = 30;
+uint8_t den = 21;
+uint8_t mesic = 4;
+uint16_t rok = 2026;
+uint8_t denvtydnu = 2;
+
+uint8_t display[POCET_SLOUPCU];
 
 void setup() {
   DDRB = 0b11111111;
@@ -38,17 +41,7 @@ void pis_vsem(uint8_t addr, uint8_t data) {
   spi_stop();
 }
 
-void pis_jednomu(uint8_t displej, uint8_t addr, uint8_t data) { //za pomocí noop napíše data jen a pouze pro námi zvolený displej
-  spi_start();
-  for(int i = pocet - 1; i> displej, i--) {
-    spi_pis(0x00, 0x00);
-  }
-  spi_pis(addr, data);
-  for(int i = displej -1; i>=0; i--) {
-    spi_pis (0x00, 0x00);
-  }
-  spi_stop();
-}
+
 
 void spi_pis(uint8_t addr, uint8_t data) {
   for(int i = 0; i < 8; i++) {
@@ -85,10 +78,6 @@ void spi_pis(uint8_t addr, uint8_t data) {
 
 void scan() {
   pis_vsem(0x0B, 0x07);
-}
-
-void smaz() {
-  
 }
 /*void test() { adresy 01 - 08 jsou pozice
   spi_pis(0x01, 0x01);
@@ -129,7 +118,7 @@ void setup_spi() {
   display_test();
 }
 
-void prehoz(int *a, int *b) {
+void prehoz(uint8_t *a, uint8_t *b) {
   *a = 0;
   (*b)++;
 }
@@ -143,17 +132,29 @@ void update_cas() {
     prehoz(&minuty, &hodiny);
   }
   if(hodiny == 25) {
+    denvtydnu++;
     prehoz(&hodiny, &den);
   }
   if(den == 31) {
     prehoz(&den, &mesic);
   }
   if(mesic == 13) {
-    prehoz(&mesic, &rok);
+    mesic = 1;
+    rok++;
   }
-} 
+  if(denvtydnu == 8) {
+    denvtydnu = 1;
+  }
+}
+
+void smaz() {
+  for(int i = 0; i < pocet_sloupcu; i++) {
+    obrazovka[i] = 0;
+  }
+}
 
 int main() {
+  char text [30];
   bool cas = true; //datum/čas přehoz
   int pocitadlo = 0;
   setup();
@@ -171,9 +172,8 @@ int main() {
     }
     while(cas == false) {
       update_cas();
-      pis_jednomu(0, 0x07, hodiny/10);
-      pis_jednomu(0, 0x02, hodiny%10);
-      pis_jednomu(1,)
+      smaz();
+
       _delay_ms(1000);
       pocitadlo++;
       if(pocitadlo == 10) {
